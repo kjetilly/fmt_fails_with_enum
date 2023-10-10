54e5a8fcb1c10d9134ccb99190f8c8412179f230
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2023-10-10 10:44:51 +0200

    Added main.

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <fmt/format.h>
+#include <fmt/core.h>
+#include <iostream>
+
+template<int ROWS, int COLS>
+struct FieldMatrix {
+    enum {
+      //! The number of rows.
+      rows = ROWS,
+      //! The number of columns.
+      cols = COLS
+    };
+};
+
+template<int ROWS, int COLS>
+struct FieldMatrixModernCPP {
+      //! The number of rows.
+  static constexpr int rows = ROWS;
+      //! The number of columns.
+  static constexpr int cols = COLS;
+};
+
+
+template<class Matrix>
+struct BlockMatrix {
+  using block_type = Matrix;
+};
+
+template<class Matrix>
+void printmatrixcompiles(const Matrix& m) {
+  std::cout << fmt::format("Rows are {}", int(Matrix::block_type::rows)) << std::endl;
+}
+
+template<class Matrix>
+void printmatrixdoesnotcompile(const Matrix& m) {
+  std::cout << fmt::format("Rows are {}", Matrix::block_type::rows) << std::endl;
+}
+
+
+int main(int, char**) {
+  BlockMatrix<FieldMatrixModernCPP<1, 1>> matrix;
+  printmatrixcompiles(matrix);
+  printmatrixdoesnotcompile(matrix);
+  return 0;
+}
+
+
